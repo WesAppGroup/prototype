2eaadfa83515c7de3684766db3c4f1899653537f
Justin Raymond <justin.raymond.14@gmail.com>
2013-11-03 23:48:44 -0500

    wesmaps course search and schedule, still waiting on server

--- assets/www/js/wesmaps.js
+++ assets/www/js/wesmaps.js
@@ -1,9 +1,50 @@
 /* Javascript for WesMaps 
  */
+/* AJAX request */
+var httpRequest;
+var coursesJSON;
+
 function startWesmaps() {
   $('#wesmaps').on('pageinit', function() {
     console.log('pageinit fired on wesmaps page');
-  });
+
+
+
+    $("#wm_search_bar").submit(function() {
+      console.log("form submitted");
+
+      httpRequest = new XMLHttpRequest();
+
+      if (!httpRequest) {
+        alert("Failed");
+        return false;
+      }
+
+      httpRequest.onreadystatechange = alertContents;
+      httpRequest.open("GET", SERVER_URL)
+      httpRequest.send();
+
+    });
+
+    function alertContents() {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          coursesJSON = httpRequest.responseText;
+          console.log(coursesJSON); 
+        }
+        else {
+          alert("Failed");
+        }
+      }
+    }
+
+    function writeCourse(course) {
+      $("wm_courses").append("<li><h3>" + course.name + "</h3><p>" + course.prof + "</p></li>");
+    }
+    $("#wm_courses").append("<li><span class='course_title'>Functional Javascript </span><span class='course_prof'>Michael Fogus</span><p>Mon, Wed, Fri 9:00-10:00am</p></li>");
+    $("#wm_courses").append("<li><span class='course_title'>Javascript: The Good Parts </span><span class='course_prof'>Douglas Crawford</span><p>Tue, Thu 9:00-10:20am</p></li>");
+    $("#wm_courses").append("<li><span class='course_title'>Javascript: The Definitive Guide </span><span class='course_prof'>David Flanagan</span><p>Mon, Wed 1:10-2:30pm</p></li>");
+      });
 
   $('#wesmaps').on('pageshow', function() {
     console.log('pageshow fired on wesmaps page');
