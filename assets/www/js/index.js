efeddd73fadd868319cd3b78472c8779f8d99ab1
Justin Raymond <justin.raymond.14@gmail.com>
2013-11-19 00:22:03 -0500

    added basic format and styling for landmarks page so that it looks like rest of app

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -36,6 +36,10 @@ $(document).ready(function() {
       $(".page").addClass("hidden");
       $("#donate").removeClass("hidden");
     }
+    else if ($(this).attr('value') === "landmarks") {
+      $(".page").addClass("hidden");
+      $("#landmarks").removeClass("hidden");
+    }
   });
 
 });
