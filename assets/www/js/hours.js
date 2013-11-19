ab1f9c57189b7b7afd9aa1d7cc641d70027a4a8d
Justin Raymond <justin.raymond.14@gmail.com>
2013-11-19 00:26:12 -0500

    nothing doing

--- assets/www/js/hours.js
+++ assets/www/js/hours.js
@@ -1,13 +1,4 @@
 /* Javascript for rss feeds
  */
 function startHours() {
-  $('#hours').on('pagecreate', function() {
-    console.log('pagecreate fired on hours');
-  });
-  $('#hours').on('pageshow', function() {
-    console.log('pageshow fired on hours');
-  });
-  $('#hours').on('pageinit', function() {
-    console.log('pageinit fired on hours');
-  });
 };
