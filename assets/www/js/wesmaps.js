ffb03591e0c5139b51ff210b2237b32ea35492b3
Justin Raymond <justin.raymond.14@gmail.com>
2013-12-01 21:07:21 -0500

    expanding info for wesmaps bufixing, still more bugs to work out

--- assets/www/js/wesmaps.js
+++ assets/www/js/wesmaps.js
@@ -1,10 +1,14 @@
 /* Javascript for WesMaps 
  */
 /* AJAX request */
-var wmHttpRequest;
+var cHttpRequest;
+var sHttpRequest;
 var coursesJSON;
+var sectionsJSON;
 var wmScroll;
 var coursesCounter = 0;
+var COURSES_URL = 'http://stumobile0.wesleyan.edu/courses/all';
+var SECTIONS_URL = 'http://stumobile0.wesleyan.edu/sections/all';
 
 
 function startWesmaps() {
@@ -14,32 +18,51 @@ function startWesmaps() {
                                              hScroll    : false,
                                              bounce     : true
                                             });
-  /* AJAX request for all the course information */
+  /* AJAX request for all the course and section information*/
   console.log("search submitted");
 
-  wmHttpRequest = new XMLHttpRequest();
+  cHttpRequest = new XMLHttpRequest();
 
-  if (!wmHttpRequest) {
+  if (!cHttpRequest) {
     alert("Failed");
     return false;
   }
 
-  wmHttpRequest.onreadystatechange = alertContents;
-  wmHttpRequest.open("GET", SERVER_URL, true)
-  wmHttpRequest.send();
+  cHttpRequest.onreadystatechange = coursesAlert;
+  cHttpRequest.open("GET", COURSES_URL, true);
+  cHttpRequest.send();
+  console.log('course request sent');
 
-  function alertContents() {
-    if (wmHttpRequest.readyState === 4) {
-      if (wmHttpRequest.status === 200) {
-        coursesJSON = $.parseJSON(wmHttpRequest.responseText);
+  sHttpRequest = new XMLHttpRequest();
+  if (!sHttpRequest) {
+    alert("Failed");
+    return false;
+  }
+  sHttpRequest.onreadystatechange = sectionsAlert;
+  sHttpRequest.open("GET", SECTIONS_URL, true);
+  sHttpRequest.send();
+  console.log('sections request sent');
+
+  function coursesAlert() {
+    if (cHttpRequest.readyState === 4) {
+      if (cHttpRequest.status === 200) {
+        coursesJSON = $.parseJSON(cHttpRequest.responseText);
       }
       else {
-        alert("Failed")
+        alert("Course request Failed");
       }
     }
   }
-  coursesJSON = $.parseJSON(COURSES_JSON);
-  sectionsJSON = $.parseJSON(SECTIONS_JSON);
+  function sectionsAlert() {
+    if (sHttpRequest.readyState === 4) {
+      if (sHttpRequest.status === 200) {
+        sectionsJSON = $.parseJSON(sHttpRequest.responseText);
+      }
+      else {
+        alert("Sections request Failed");
+      }
+    }
+  }
 
   $(document).on("click","#wm_icon", function() {
     coursesCounter = 0;
@@ -52,16 +75,13 @@ function startWesmaps() {
     for (var c in coursesJSON) {
 
       if (coursesCounter < 10) {  //limit to 10 results
-        //console.log(c);
-        //console.log(coursesJSON[c].value);
-        //console.log(coursesJSON[c].value.courseTitle);
         if (searchRE.test(coursesJSON[c].value.courseTitle)) {
           writeCourse(coursesJSON[c]);
           coursesCounter++;
         }
       }
       else {
-        break
+        break;
       }
     }
     setTimeout(function() {
@@ -106,12 +126,11 @@ function startWesmaps() {
   }
 
   $(document).on("click",".wm_c_expand", function() {
-      var li = $(this).parent().parent();
-      var info = $(this).parent();
-      var key = $(this).data().key;
-      var sect;
-      var crse
-
+    var li = $(this).parent().parent();
+    var info = $(this).parent();
+    var key = $(this).data().key;
+    var sect;
+    var crse;
     if ($(this).parent().children('.wm_c_expand').children('i').hasClass('fa-plus-square-o')) {
       console.log($(this).data());
       for (var s in sectionsJSON) {
@@ -131,11 +150,8 @@ function startWesmaps() {
       info.children('table').children('tbody').children('tr').children('.wm_c_time').html(sect.sectionTime);
       info.children('table').children('tbody').children('tr').children('.wm_c_loc').html(sect.sectionLocation);
       info.children('table').children('tbody').children('tr').children('.wm_c_seats').html('seats: ' + sect.sectionSeats_available);
-
       info.children('table').removeClass('hidden');
-
       info.children('.wm_c_expand').html("<i class='fa fa-minus-square-o fa-3x'></i>");
-
       info.addClass('wm-info-expanded');
     }
     else {
@@ -144,21 +160,18 @@ function startWesmaps() {
       info.children('table').addClass('hidden');
 
     }
-
     setTimeout(function() {
       wmScroll.refresh();
     }, 0);
   });
-});
-
+}
 /* Parses string from course JSON in the form
   {instructor=\u003eemoran, first_name=\u003eEdward, last_name=\u003eMoran}
 */
 function parseProf (prof) {
-  var re = /.*?\u003e(.*?),.*?\u003e(.*?),.*?\u003e(.*?)\}/
+  var re = /.*?\u003e(.*?),.*?\u003e(.*?),.*?\u003e(.*?)\}/;
   var m = re.exec(prof);
   var fName = m[2];
   var lName = m[3];
   return fName + ' ' + lName;
->>>>>>> af9b20c... wesmaps courses expand to show more info when clicked
 }
