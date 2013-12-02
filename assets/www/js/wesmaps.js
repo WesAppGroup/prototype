b5494b54c191934038ea4c530192376996f024c9
Justin Raymond <justin.raymond.14@gmail.com>
2013-12-01 22:25:27 -0500

    merge

--- assets/www/js/wesmaps.js
+++ assets/www/js/wesmaps.js
@@ -76,7 +76,6 @@ function startWesmaps() {
   });
 
   function writeCourse(c) {
-    coursesCounter++;
     $("#wm_courses").append("<li><div class='wm_c_dnum'>" +
                               "<div class='wm_c_dep'>" +
                               c.value.courseDepartment +
@@ -92,7 +91,8 @@ function startWesmaps() {
                               "<div class='wm_c_expand' id='wm_c_" + c.key + "'>" +
                               "<i class='fa fa-plus-square-o fa-3x'></i>" +
                               "</div>" +
-                              "<table class='hidden'>" +
+                              "<div class='wm-table hidden'>" +
+                              "<table>" +
                               "<tr>" +
                               "<td class='wm_c_prof'></td>" +
                               "<td class='wm_c_time'></td>" +
@@ -105,6 +105,7 @@ function startWesmaps() {
                               "<td class='wm_c_desc'></td>" +
                               "</tr>" + 
                               "</table>" + 
+                              "</div>" +
                               "</div>" + 
                               "</li>"
                             );
@@ -131,19 +132,21 @@ function startWesmaps() {
           break;
         }
       }
-      info.children('table').children('tbody').children('tr').children('.wm_c_desc').html(crse.courseDescription);
-      info.children('table').children('tbody').children('tr').children('.wm_c_prof').html(parseProf(sect.sectionProfessors)); 
-      info.children('table').children('tbody').children('tr').children('.wm_c_time').html(sect.sectionTime);
-      info.children('table').children('tbody').children('tr').children('.wm_c_loc').html(sect.sectionLocation);
-      info.children('table').children('tbody').children('tr').children('.wm_c_seats').html('seats: ' + sect.sectionSeats_available);
-      info.children('table').removeClass('hidden');
+      info.children('.wm-table').children('table').children('tbody').children('tr').children('.wm_c_desc').html(crse.courseDescription);
+      info.children('.wm-table').children('table').children('tbody').children('tr').children('.wm_c_prof').html(parseProf(sect.sectionProfessors)); 
+      info.children('.wm-table').children('table').children('tbody').children('tr').children('.wm_c_time').html(sect.sectionTime);
+      info.children('.wm-table').children('table').children('tbody').children('tr').children('.wm_c_loc').html(sect.sectionLocation);
+      info.children('.wm-table').children('table').children('tbody').children('tr').children('.wm_c_seats').html('seats: ' + sect.sectionSeats_available);
+
+      info.children('.wm-table').removeClass('hidden');
+
       info.children('.wm_c_expand').html("<i class='fa fa-minus-square-o fa-3x'></i>");
       info.addClass('wm-info-expanded');
     }
     else {
       info.children('.wm_c_expand').html("<i class='fa fa-plus-square-o fa-3x'></i>");
       info.removeClass('wm-info-expanded');
-      info.children('table').addClass('hidden');
+      info.children('.wm-table').addClass('hidden');
 
     }
   });
@@ -152,9 +155,17 @@ function startWesmaps() {
   {instructor=\u003eemoran, first_name=\u003eEdward, last_name=\u003eMoran}
 */
 function parseProf (prof) {
-  var re = /.*?\u003e(.*?),.*?\u003e(.*?),.*?\u003e(.*?)\}/;
-  var m = re.exec(prof);
-  var fName = m[2];
-  var lName = m[3];
-  return fName + ' ' + lName;
+  var fName;
+  var lName;
+  var m;
+  var re = /.*?\u003e(.*?),.*?\u003e(.*?),.*?\u003e(.*?)\}/
+  if (re.test(prof)) {
+    m = re.exec(prof);
+    fName = m[2];
+    lName = m[3];
+    return fName + ' ' + lName;
+  }
+  else {
+    return prof;
+  }
 }
