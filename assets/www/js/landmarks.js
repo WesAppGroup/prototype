40604835a156715615aef6a8207fb2562ed7a55d
lilywes <ygu01@wesleyan.edu>
2013-12-04 20:24:23 -0500

    Update landmarks.js

--- assets/www/js/landmarks.js
+++ assets/www/js/landmarks.js
@@ -1,18 +1,19 @@
-var counter;
+var counter = 0;
 // update when server is ready
 var landmarks_data_url = "?????????????????????";
 
 
 $(document).ready(function() {
+
   try {
     console.log("attempting to fetch landmarks data from server")
       $.get(
-	landmarks_data_url, function(data) {
-	  landmarks_JSON = data
+				landmarks_data_url, function(data) {
+	  			landmarks_JSON = data
         }
       );
       if (landmarks_JSON == undefined){
-	throw err
+				throw err
       }
   } catch (err) {
     var landmarks_JSON = '[{"value":{"longitude":"-72.654283", "facility_id":"F_336", "latitude":"41.554999", "search_terms":"200 High St, Eclectic, Eclectic Society, Phi Nu Theta, fraternity", "url":null, "name":"Eclectic", "id":"P_385", "description":"200 High Street", "campus_map_building_id":null},"key":1}, {"value":{ "longitude":null, "facility_id":"F_46", "latitude":null, "search_terms":"Winchester House, anthropology, Anthropology Department", "url":null, "name":"Anthropology", "id":"P_387", "description":"Winchester House", "campus_map_building_id":null}, "key":2}]';
@@ -36,6 +37,7 @@ $(document).ready(function() {
         }
       }
     });
+	};
 
   function writeLandmarks(c) {
     $("#lm_list").append("<li><div class='lm_c_dnum'>" +
@@ -57,7 +59,7 @@ $(document).ready(function() {
                               "</li>"
                             );
   }
-
 });
+
 
 		
