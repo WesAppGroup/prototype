bf18ce07206b45470db79471f4170da83475932b
lilywes <ygu01@wesleyan.edu>
2013-12-04 20:11:53 -0500

    added url retrievel; waiting for server

--- assets/www/js/landmarks.js
+++ assets/www/js/landmarks.js
@@ -1,27 +1,41 @@
-var counter = 0;
-var landmarks_JSON = '[{"value":{"longitude":"-72.654283", "facility_id":"F_336", "latitude":"41.554999", "search_terms":"200 High St, Eclectic, Eclectic Society, Phi Nu Theta, fraternity", "url":null, "name":"Eclectic", "id":"P_385", "description":"200 High Street", "campus_map_building_id":null},"key":1}, {"value":{ "longitude":null, "facility_id":"F_46", "latitude":null, "search_terms":"Winchester House, anthropology, Anthropology Department", "url":null, "name":"Anthropology", "id":"P_387", "description":"Winchester House", "campus_map_building_id":null}, "key":2}]';
+var counter;
+// update when server is ready
+var landmarks_data_url = "?????????????????????";
+
 
 $(document).ready(function() {
-  landmarksJSON = $.parseJSON(landmarks_JSON);
+  try {
+    console.log("attempting to fetch landmarks data from server")
+      $.get(
+	landmarks_data_url, function(data) {
+	  landmarks_JSON = data
+        }
+      );
+      if (landmarks_JSON == undefined){
+	throw err
+      }
+  } catch (err) {
+    var landmarks_JSON = '[{"value":{"longitude":"-72.654283", "facility_id":"F_336", "latitude":"41.554999", "search_terms":"200 High St, Eclectic, Eclectic Society, Phi Nu Theta, fraternity", "url":null, "name":"Eclectic", "id":"P_385", "description":"200 High Street", "campus_map_building_id":null},"key":1}, {"value":{ "longitude":null, "facility_id":"F_46", "latitude":null, "search_terms":"Winchester House, anthropology, Anthropology Department", "url":null, "name":"Anthropology", "id":"P_387", "description":"Winchester House", "campus_map_building_id":null}, "key":2}]';
+    landmarksJSON = $.parseJSON(landmarks_JSON);
 
-  $(document).on("click","#wm_icon", function() {
-    counter = 0;
-    $("#lm_list").empty();
+    $(document).on("click","#wm_icon", function() {
+      counter = 0;
+      $("#lm_list").empty();
 
-    var search = $("#lm_bar > input").val();
-    var searchRE = new RegExp(search, 'i');
-    for (var c in landmarksJSON) {
-      if (counter < 10) {  //limit to 10 results
-        if (searchRE.test(landmarksJSON[c].value.search_terms)) {
-          writeLandmarks(landmarksJSON[c]);
-          counter++;
+      var search = $("#lm_bar > input").val();
+      var searchRE = new RegExp(search, 'i');
+      for (var c in landmarksJSON) {
+        if (counter < 10) {  //limit to 10 results
+          if (searchRE.test(landmarksJSON[c].value.search_terms)) {
+            writeLandmarks(landmarksJSON[c]);
+            counter++;
+          }
+        }
+        else {
+          break
         }
       }
-      else {
-        break
-      }
-    }
-  });
+    });
 
   function writeLandmarks(c) {
     $("#lm_list").append("<li><div class='wm_c_dnum'>" +
