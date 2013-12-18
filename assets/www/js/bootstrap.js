7844f93a2a2c37a33c8cb9e3a9ff4568005fa410
Justin Raymond <justin.raymond.14@gmail.com>
2013-12-17 22:33:06 -0500

    retooled hours and menus into the same page for smoother transition

--- assets/www/js/bootstrap.js
+++ assets/www/js/bootstrap.js
@@ -24,6 +24,6 @@
   };
 
   // IF DEVELOPING ON LOCAL ENABLE FIRST THING HERE
-  // $(document).ready(onDeviceReady)
+   //$(document).ready(onDeviceReady)
   document.addEventListener("deviceready", onDeviceReady, false);
 })();
